4f097008fb0c2c3dfe7c593a90cfe86514606d05
takanoharuka <haruka.takano0111@gmail.com>
2020-07-21 19:28:36 +0900

    Asynchronous_communication_exit

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="message_box">
+          <div class="message_info">
+            <div class="message_info__username">
+              ${message.user_name}
+            </div>
+            <div class="message_info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message">
+            <p class="message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="message_box">
+        <div class="message_info">
+          <div class="message_info__username">
+            ${message.user_name}
+          </div>
+          <div class="message_info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p class="message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+  $('.Form').on('submit', function(e){
+    console.log("OK")
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log(data)
+      let html = buildHTML(data);
+      $('.chat_main__message').append(html);
+      $('form')[0].reset();
+      $('.chat_main__message').animate({ scrollTop: $('.chat_main__message')[0].scrollHeight});
+      $('.submit_btn').prop('disabled',false);
+    })
+    .fail(function(){
+          alert('メッセージ送信に失敗しました');
+    });
+  });
+});
